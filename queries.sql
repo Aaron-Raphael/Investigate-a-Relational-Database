479c93c74db6a591eff7f8f69acbfdb4d979ba83
Aaron Raphael H J <aaronraphaelhj@gmail.com>
2020-05-07 10:58:23 +0530

    feat : Add SQL code for Question 2
    Write a query that returns the store ID for the store,
     the year and month and the number of rental orders each store has fulfilled for that month.

--- queries.sql
+++ queries.sql
@@ -21,3 +21,21 @@ SELECT category, standard_quartile, COUNT(rental_duration) AS count
 FROM f_category
 GROUP BY 1, 2
 ORDER BY 1, 2
+
+/*
+Question 2:
+ Write a query that returns the store ID for the store, 
+ the year and month and the number of rental orders each store has fulfilled for that month. 
+ Your table should include a column for each of the following: 
+ year, month, store ID and count of rental orders fulfilled during that month
+*/
+
+SELECT DATE_PART('month',r.rental_date) AS rental_month, 
+    DATE_PART('year',r.rental_date) AS rental_year, 
+    s.store_id AS store_id,
+count(*) AS count_rentals
+FROM rental AS r
+JOIN staff AS s
+ON r.staff_id = s.staff_id
+GROUP BY 1,2,3
+ORDER BY 4 DESC
